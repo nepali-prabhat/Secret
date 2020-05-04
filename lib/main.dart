6d6d7951f733e157dce9963a512255b90187a77e
mallaneha <nehamalla99@gmail.com>
2020-05-04 18:07:25 +0545

    weekly dates & task component

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        fontFamily: 'Quicksand',
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Secret'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +28,256 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var thisInstant = new DateTime.now();
+  List<Map<int, String>> weekList = weakling();
+  List<Map<int, String>> detailList = [
+    { 0: 'Start the Secret flutter project', 1: 'Secret', 2: '2', 3: '90', 4: '0.60' },
+    { 0: 'All the bright places', 1: 'Novel', 2: '6', 3: '70', 4: '0.70' },
+    { 0: 'be a little smart', 1: 'dummy dumb neha', 2: '9', 3: '30', 4: '0.30' },
+  ];
+  final String customIcon1 = 'assets/images/icon1.svg';
+  final String customIcon2 = 'assets/images/icon2.svg';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child:Column(
+          children: [
+            Container(
+              height: 70,
+              width: 360,
+              margin: const EdgeInsets.fromLTRB(16, 12, 16, 24),
+              padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+
+              child: Row(
+                children: [
+                  new Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: weekList.length,
+                      itemBuilder: (BuildContext context, int index){
+                        return Container(
+                          height: 70,
+                          width: 40,
+                          margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                          decoration: BoxDecoration(
+                            color: weekList[index][1] == thisInstant.day.toString() ? Color.fromRGBO(249, 209, 4, 0.25) : Color.fromRGBO(255, 255, 255, 0.0),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(weekList[index][0],
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: Color.fromRGBO(0, 0, 0, 1.0),
+                                    fontFamily: 'Quicksand',
+                                    fontWeight: weekList[index][1] == thisInstant.day.toString() ? FontWeight.bold : FontWeight.normal,
+                                ),
+                              ),
+                              Text(weekList[index][1],
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: Color.fromRGBO(0, 0, 0, 1.0),
+                                    fontFamily: 'Quicksand',
+                                    fontWeight: weekList[index][1] == thisInstant.day.toString() ? FontWeight.bold : FontWeight.normal,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    )
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new Expanded(
+              child: ListView.builder(
+                itemCount: detailList.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Stack(
+                    children: [
+                      Container(
+                        height: 70,
+                        width: 380,
+                        margin: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 1.0),
+                          borderRadius: BorderRadius.circular(20.0),
+                          boxShadow: [
+                            new BoxShadow(
+                              color: Color.fromRGBO(0, 0, 0, 0.05),
+                              offset: new Offset(0.0, 10.0),
+                              blurRadius: 20.0,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 70,
+                        width: 380 * double.parse(detailList[index][4]),
+                        margin: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(249, 209, 4, 1.0),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20.0),
+                            bottomLeft: Radius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+                        padding: const EdgeInsets.fromLTRB(24, 16, 16, 16),
+
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                              height: 38,
+                              width: 278,
+                              margin: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(detailList[index][0],
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Color.fromRGBO(0, 0, 0, 1.0),
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: 'Quicksand'
+                                    ),
+                                  ),
+                                  Text(detailList[index][1],
+                                    style: TextStyle(
+                                      height: 1.85,
+                                      fontSize: 12,
+                                      color: Color.fromRGBO(31, 28, 12, 0.5),
+                                      fontFamily: 'Quicksand'
+                                    )
+                                  ),
+                                ]
+                              ),
+                            ),
+
+                            SvgPicture.asset(
+                              customIcon1,
+                              placeholderBuilder: (BuildContext context) => Container(
+                                height: 10,
+                                width: 10,
+                              ),
+                            ),
+
+                            Container(
+                              height: 10,
+                              width: 10,
+                              margin: const EdgeInsets.fromLTRB(2, 0, 8, 0),
+
+                              child:
+                              Text(detailList[index][2],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  color: Color.fromRGBO(0, 0, 0, 1.0),
+                                  fontFamily: 'Quicksand'
+                                )
+                              ),
+                            ),
+
+                            SvgPicture.asset(
+                              customIcon2,
+                              placeholderBuilder: (BuildContext context) => Container(
+                                height: 10,
+                                width: 10,
+                              ),
+                            ),
+
+                            Container(
+                              height: 10,
+                              width: 10,
+                              margin: const EdgeInsets.fromLTRB(5, 0, 1, 0),
+
+                              child:
+                              Text(detailList[index][3],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  color: Color.fromRGBO(0, 0, 0, 1.0),
+                                  fontFamily: 'Quicksand'
+                                )
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]
+                  );
+                }
+              )
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+
+weakling(){
+  int todayIndex;
+  var thisInstant = new DateTime.now();
+  List<Map<int, String>> weekList = [
+    {0: 'S', 1: '0'}, {0: 'M', 1: '0'}, {0: 'T', 1: '0'}, {0: 'W', 1: '0'}, {0: 'T', 1: '0'}, {0: 'F', 1: '0'}, {0: 'S', 1: '0'},
+  ];
+
+  for(int i = 1; i<8; i++){
+    if(i == thisInstant.weekday){
+      if(thisInstant.weekday == 7){
+        weekList[0][1] = thisInstant.day.toString();
+        todayIndex = 0;
+      }
+      else{
+        weekList[i][1] = thisInstant.day.toString();
+        todayIndex = i;
+      }
+    }
+  }
+
+  int j = todayIndex;
+  int k = todayIndex;
+  int todayJ = thisInstant.day;
+  int todayK = thisInstant.day;
+  int month = thisInstant.month;
+
+  while(j > 0){
+    if(todayJ == 1){
+      if(month == 5 || month == 7 || month == 10 || month == 12 ){
+        todayJ = 31;
+      }
+      else if(month == 3){
+        todayJ = 29;
+      }
+      else{
+        todayJ = 32;
+      }
+    }
+    weekList[j-1][1] = (--todayJ).toString();
+    j--;
+  }
+
+  while(k != 6){
+    if((todayK == 31)||(month == 2 && todayK == 28)||(todayK == 30 && (month == 4 || month == 6 || month == 9 || month == 11))){
+      todayK = 0;
+    }
+    weekList[k+1][1] = (++todayK).toString();
+    k++;
+  }
+
+  return weekList;
 }
